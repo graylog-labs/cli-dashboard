897c6440064c6f21b4d2c78996d4c462f31b0f0e
Samuel Reed <samuel.trace.reed@gmail.com>
2016-10-31 12:47:02 -0500

    Rework options parsing and help text. Allow specifying all options via CLI.

--- graylog-dashboard.js
+++ graylog-dashboard.js
@@ -18,46 +18,53 @@
  * along with Graylog2.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const fs = require('fs');
-const yaml = require('js-yaml');
-const argv = require('yargs')
-    .usage('Usage: graylog-dashboard --stream-id [stream-id] --host [graylog-server REST API URL]')
-    .demand(['h'])
-    .alias('s', 'stream-id')
-    .alias('h', 'host')
-    .argv;
-
 const Promise = require('bluebird');
 const graylog = require('./lib/graylog-api');
 const ui = require('./lib/screen');
 const handlers = require('./lib/handlers');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const yargs = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .describe('stream-id', 'Graylog Stream ID')
+  .describe('host', 'Full Graylog REST API URL')
+  .describe('poll-interval', 'How often (in ms) to poll the Graylog server')
+  .default('poll-interval', 1000)
+  .describe('username', 'Graylog username')
+  .describe('password', 'Graylog password')
+  .describe('cred-file-path', 'Path to an optional credentials file')
+  .default('cred-file-path', `${process.env.HOME}/.graylog_dashboard`)
+  .help();
+const {argv} = yargs;
 
-// CLI arguments
-let serverURL = argv.h;
+// Read user credentials.
+let fileConfig = {};
+const {'cred-file-path': credFilePath} = argv;
+try {
+  fileConfig = yaml.safeLoad(fs.readFileSync(argv.credFilePath, 'utf8'));
+} catch (err) {
+  // Ignore, maybe everything was defined in argv
+}
+
+// Merge argv & file config.
+const config = Object.assign({}, fileConfig, argv);
+
+// Check mandatory config.
+['username', 'password', 'host'].forEach((k) => {
+  if (!config[k]) {
+    console.error(`Error: No ${k} defined in ${credFilePath} or arguments!`);
+    yargs.showHelp();
+    process.exit(1);
+  }
+});
+
+// eslint-disable-next-line prefer-const
+let {username, password, 'stream-id': streamID, host: serverURL, 'poll-interval': pollInterval} = config;
 
 // Make sure serverURL has a trailing slash. (computers.)
 if (serverURL[serverURL.length - 1] !== '/') serverURL += '/';
 
-// Read user credentials.
-const credFilePath = process.env['HOME'] + "/.graylog_dashboard";
-let config;
-try {
-  config = yaml.safeLoad(fs.readFileSync(credFilePath, 'utf8'));
-} catch (err) {
-  throw new Error(`Could not read Graylog user credentials file at ${credFilePath} - Please create it ` +
-                  `as described in the README. (${err.message})`);
-}
-
-// Check config.
-['username', 'password'].forEach((k) => {
-  if (!config[k]) throw new Error(`No ${k} defined in ${credFilePath}.`);
-});
-
-const {username, password, poll_interval = 1000} = config;
-
-let streamID = argv.s; // may be null
 let streams; // filled in after streams runs
-
 function storeStreams(_streams) {
   streams = _streams;
   if (!streamID) streamID = streams[0].id;
@@ -78,7 +85,7 @@ function poll() {
     graylog.streamAlerts({serverURL, streamID, username, password}).then(handlers.renderAlerts),
   ])
   .then(() => ui.render()) // batch up renders
-  .delay(poll_interval)
+  .delay(pollInterval)
   .then(() => poll(streamID));
 }
 
